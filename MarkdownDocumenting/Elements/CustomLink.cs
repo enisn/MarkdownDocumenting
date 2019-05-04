36a059ed33e575c5c261e63ff30aceebf0d7d961
enisn <iletisim@enisnecipoglu.com>
2019-05-04 23:00:28 +0300

    Added OpenInNewWindow to CustomLinks

---
+++ MarkdownDocumenting/Elements/CustomLink.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarkdownDocumenting.Elements
+{
+
+    public class CustomLink
+    {
+        public CustomLink()
+        {
+        }
+
+        public CustomLink(string display, string url, bool openInNewWindow = false)
+        {
+            Display = display;
+            Url = url;
+        }
+
+        public string Display { get; set; }
+        public string Url { get; set; }
+        public bool OpenInNewWindow { get; set; }
+    }
+}
---
+++ MarkdownDocumenting/Elements/FooterMetaData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarkdownDocumenting.Elements
+{
+    public class FooterMetaData
+    {
+        public string Link { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
